980c45112b6ca8e49f86929a380c530d8ffd9750
Johnnyprox <janko.sevcja@gmail.com>
2021-11-15 18:45:27 +0100

    test cunction

--- pokus1.m
+++ pokus1.m
@@ -3,18 +3,30 @@ close all
 clc
 
 filePath = pwd
+team = "CZE"
+year  = 2000
 
 
 if ~isfolder( filePath )
         error('Folder does not exist.')
-    end
+end
+
+
+
+
         
 inputData = readtable([ filePath '/' 'dataOH.csv' ]); %Načtení datového souboru 
 numberRecords = size( inputData, 1 ); % Změnit dle nutnosti  
 
 
-DataSeasonS = inputData(inputData.Season == "Summer",:);
-DataSeasonW = inputData(inputData.Season == "Winter",:);
+date = inputData(inputData.Year == year,:);
+
+
+DataSeasonS = date(date.Season == "Summer",:);
+DataSeasonW = date(date.Season == "Winter",:);
+
+
+
 
 
 teamS = DataSeasonS.Team;
@@ -23,10 +35,18 @@ teamW = DataSeasonW.Team;
 TeamnameS = unique(teamS);
 TeamnamesW = unique(teamW);
 
-s = TeamnameS(2)
 
 
-DataTeamS = DataSeasonS(DataSeasonS.Team == "Czech Republic",:);
+
+DataTeamS = DataSeasonS(DataSeasonS.NOC == team,:);
+
+
+
+
+[Gold,x] = size(DataTeamS(DataTeamS.Medal == "Gold","ID"));
+[Silver,x] = size(DataTeamS(DataTeamS.Medal == "Silver","ID"));
+[Bronze,x] = size(DataTeamS(DataTeamS.Medal == "Bronze","ID")); 
+[countFull,x] = size(DataTeamS);
 
 
 
