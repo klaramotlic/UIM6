9d2894f345d88105ec598b40f6871eb42f1010c8
Johnnyprox <janko.sevcja@gmail.com>
2021-11-15 18:45:38 +0100

    main function

--- MyModel.m
+++ MyModel.m
@@ -5,19 +5,25 @@ function [ targetSeries, predictedSeries ] = MyModel( data )
 %Vstup:             data:           vstupní surová data reprezentující 1
 %objekt (1 èasovou øadu, 1 pacienta, 1 obrázek, apod.). 
 
+[gold, silver, bronze, all] = sortingData(data, "CZE", 2000);   
+
+targetSeries = [gold, silver, bronze, all];
+
+
+
 %Výstup:            targetSeries:         "èasová øada" zaznamenaných poètù
 %medaili (dle databáze) v jednotlivých sezonách
 %                   predictedSeries:      "èasová øada" predikovaných poètù medaili dle algoritmu
 
-targetSeries = GetTargetSeries( ... ); % doplnit potøebné vstupní promìnné
-predictedSeries = [];
-
-end
-
-function targetSeries = GetTargetSeries( input )
-%Do této funkce doplòte algoritmus pro urèení výstupní èasové sekvence
-%poètù medaili ze surových dat, dostupných pro jednotlivé jedince.
-
-    targetSeries = [];
+% targetSeries = GetTargetSeries( ... ); % doplnit potøebné vstupní promìnné
+% predictedSeries = [];
+% 
+% end
+% 
+% function targetSeries = GetTargetSeries( input )
+% %Do této funkce doplòte algoritmus pro urèení výstupní èasové sekvence
+% %poètù medaili ze surových dat, dostupných pro jednotlivé jedince.
+% 
+%     targetSeries = [];
 
 end
